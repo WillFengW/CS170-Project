7af9cb3e68adfaed4da45c5c00cca2be99dacefc
WwwwwFeng <wfeng026@ucr.edu>
2023-05-02 20:50:07 -0700

    operator, frontier, expndNode, childrenCreator

--- defaultPuzzle.py
+++ defaultPuzzle.py
@@ -1,11 +1,88 @@
 import numpy as np
+import re
 
 class defaultPuzzle():
     def __init__(self):
         self.initialState = np.array([[1,2,0],[4,5,3],[7,8,6]])
+        self.goalState = np.array([[1,2,3],[4,5,6],[7,8,0]])
+        self.expandedNode = np.zeros((3,3))
+        self.frontier = []
         
+    # print the puzzle in expanded node
     def printPuzzle(self):
         print("The Puzzle:")
-        print(self.initialState[0])
-        print(self.initialState[1])
-        print(self.initialState[2])
+        print(self.expandedNode[0])
+        print(self.expandedNode[1])
+        print(self.expandedNode[2])
+        
+    # get the initial frontier, run before everything
+    def initialFrontier(self):
+        self.frontier.append(self.initialState)
+        
+    # Test if the node equal to the goal state
+    def goalTest(self) -> bool:
+        if (np.array_equal(self.expandedNode, self.goalState)): return True
+        
+    # get the first array from frontier
+    def removeFront(self):
+        if (self.frontier): self.expandedNode = self.frontier.pop(0)
+        else: print("The Frontier is Empty!\n")
+        
+    # push any children nodes to frontier
+    def pushFrontier(self, children: list):
+        self.frontier.extend(children)
+        
+    # find the index of number 0, you should not use this directly
+    def findIndex(self):
+        return np.where(self.initialState == 0)
+        
+    # move 0 up, you should not use this directly
+    def moveUp(self, y: int, x: int):
+        tmp = np.copy(self.expandedNode)
+        tmp[y][x], tmp[y-1][x] = tmp[y-1][x], tmp[y][x]
+        return tmp
+    
+    # move 0 down, you should not use this directly
+    def moveDown(self, y: int, x: int):
+        tmp = np.copy(self.expandedNode)
+        tmp[y][x], tmp[y+1][x] = tmp[y+1][x], tmp[y][x]
+        return tmp
+    
+    # move 0 right, you should not use this directly
+    def moveRight(self, y: int, x: int):
+        tmp = np.copy(self.expandedNode)
+        tmp[y][x], tmp[y][x+1] = tmp[y][x+1], tmp[y][x]
+        return tmp
+    
+    # move 0 left, you should not use this directly
+    def moveLeft(self, y: int, x: int):
+        tmp = np.copy(self.expandedNode)
+        tmp[y][x], tmp[y][x-1] = tmp[y][x-1], tmp[y][x]
+        return tmp
+
+    # find any possible children of expanded node
+    def createChildren(self):
+        indexs = self.findIndex()
+        (y, x) = indexs[0][0], indexs[1][0]
+        children = []
+        
+        if (y == 0):
+            children.append(self.moveDown(y, x))
+        if (y == 1):
+            children.append(self.moveUp(y, x))
+            children.append(self.moveDown(y, x))
+        if (y == 2):
+            children.append(self.moveUp(y, x))
+        
+        if (x == 0):
+            children.append(self.moveRight(y, x))
+        if (x == 1):
+            children.append(self.moveLeft(y, x))
+            children.append(self.moveRight(y, x))
+        if (x == 2):
+            children.append(self.moveLeft(y, x))
+        
+        return children
+    
+if __name__ == "__main__":
+    d = defaultPuzzle()
--- puzzleCollector.py
+++ puzzleCollector.py
@@ -6,6 +6,7 @@ class puzzleCollector(defaultPuzzle):
     def __init__(self):
         super().__init__()
         
+    # get the user input
     def setPuzzle(self):
         print("Enter your puzzle, use a zero to represent the blank")
         row = input("Enter the first row, use space or tabs between numbers: ")
@@ -14,8 +15,6 @@ class puzzleCollector(defaultPuzzle):
         self.initialState[1] = [int(i) for i in re.findall(r'\b\d+\b', row)]
         row = input("Enter the third row, use space or tabs between numbers: ")
         self.initialState[2] = [int(i) for i in re.findall(r'\b\d+\b', row)]
-        
-    #def checkPuzzle(self):
-    #   check if the puzzle is valid
+
         
         
